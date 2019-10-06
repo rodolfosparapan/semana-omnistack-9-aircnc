18d1a5ed618e1c2c2caed4daa50f6ab652087e5f
Rodolfo Sparapan <sparapan@live.com>
2019-10-06 16:15:13 -0300

    Adicionado conexao mongoAtlas

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,8 @@ const path = require('path');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/aircnc', {
+// local: mongodb://localhost:27017/aircnc
+mongoose.connect('mongodb+srv://sparapan:sparapan@cluster0-ncstq.mongodb.net/aircnc?retryWrites=true&w=majority', {
    useNewUrlParser: true,
    useUnifiedTopology: true 
 });
