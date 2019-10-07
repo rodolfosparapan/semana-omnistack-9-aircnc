f52eca78d3b9bb28dc4ca5d59aea378ac299b5d6
Rodolfo Sparapan <rodolfosparapan@gmail.com>
2019-10-06 22:34:07 -0300

    Adicionando socket.io

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./routes');
 const cors = require('cors');
 const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
+
+const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 // local: mongodb://localhost:27017/aircnc
 mongoose.connect('mongodb+srv://sparapan:sparapan@cluster0-ncstq.mongodb.net/aircnc?retryWrites=true&w=majority', {
@@ -12,8 +17,22 @@ mongoose.connect('mongodb+srv://sparapan:sparapan@cluster0-ncstq.mongodb.net/air
    useUnifiedTopology: true 
 });
 
+const connectedUsers = {};
+io.on('connection', socket => {
+   const { user_id } = socket.handshake.query;
+   console.log("user:" +user_id, "socket:"+socket.id);
+   connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+   req.io = io;
+   req.connectedUsers = connectedUsers;
+   return next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
-app.listen(3333);
+
+server.listen(3333);
